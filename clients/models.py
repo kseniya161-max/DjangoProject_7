f2e091f07a996396f33664c94dad8530ae51fe97
Kseniya <baharevaxen@yandex.ru>
2025-11-13 17:12:02 +0300

    create models

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Clients(models.Model):
+    email = models.EmailField(max_length=100, unique=True)
+    name = models.CharField(max_length = 100)
+    comment = models.TextField(blank=True)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Message(models.Model):
+    header = models.CharField(max_length=200)
+    content = models.TextField()
+
+
+    def __str__(self):
+        return self.header
