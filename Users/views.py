2ef739a0d89cfea6a3fba84c25be34ebc1d79bba
Kseniya <baharevaxen@yandex.ru>
2025-11-22 15:04:44 +0300

    creating views for register

--- Users/views.py
+++ Users/views.py
@@ -1,3 +1,21 @@
+from django.contrib import messages
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from Users.forms import UserRegisterForm
+from Users.models import User
+
+
+class CreateUserView(CreateView):
+    """Создаем представление для регистрации"""
+    model = User
+    form_class = UserRegisterForm
+    template_name = 'register.html'
+    success_url = reverse_lazy('Users:login')
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Ваш аккаунт был успешно создан! Теперь Вы можете войти.')
+        return super().form_valid(form)
+
+
